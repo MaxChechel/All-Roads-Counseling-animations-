e97db1254060cba8c23e62ebac073d379068578a
Max Chechel <maksymchechel@yahoo.com>
2024-03-19 09:32:13 +0100

    updates

--- dist/home/index.js
+++ dist/home/index.js
@@ -589,10 +589,34 @@ var _gsap = require("gsap");
 var _gsapDefault = parcelHelpers.interopDefault(_gsap);
 var _scrollTrigger = require("gsap/ScrollTrigger");
 var _motionPathPlugin = require("gsap/MotionPathPlugin");
+var _imagesloaded = require("imagesloaded");
+var _imagesloadedDefault = parcelHelpers.interopDefault(_imagesloaded);
 (0, _gsapDefault.default).registerPlugin((0, _scrollTrigger.ScrollTrigger), (0, _motionPathPlugin.MotionPathPlugin));
+const svgs = document.querySelectorAll(".svg-parent");
+function setViewBox(svg, svgPath) {
+    const svgContainer = svg.parentElement;
+    const svgWidth = svgContainer.clientWidth;
+    const svgHeight = svgContainer.clientHeight;
+    // Get the bounding box of the SVG path
+    const pathBounds = svgPath.getBBox();
+    const pathAspectRatio = pathBounds.width / pathBounds.height;
+    // Calculate the viewBox dimensions based on the aspect ratio of the path
+    let viewBoxWidth, viewBoxHeight;
+    if (svgWidth / svgHeight > pathAspectRatio) {
+        viewBoxWidth = pathBounds.width;
+        viewBoxHeight = viewBoxWidth * (svgHeight / svgWidth);
+    } else {
+        viewBoxHeight = pathBounds.height;
+        viewBoxWidth = viewBoxHeight * (svgWidth / svgHeight);
+    }
+    // Set the viewBox attribute of the SVG
+    svg.setAttribute("viewBox", `${pathBounds.x} ${pathBounds.y} ${viewBoxWidth} ${viewBoxHeight}`);
+    console.log("viewbox set");
+}
 function initMotionPath() {
     // Register MotionPathPlugin here to avoid multiple registrations
     (0, _gsapDefault.default).registerPlugin((0, _motionPathPlugin.MotionPathPlugin), (0, _scrollTrigger.ScrollTrigger));
+    (0, _scrollTrigger.ScrollTrigger).normalizeScroll(true);
     // declare an array to store tweens
     let tweens = [];
     // function to create and manage tweens
@@ -605,13 +629,19 @@ function initMotionPath() {
         // iterate over each SVG
         svgs.forEach((svg, i)=>{
             let startPosition = "0%";
-            i > 0 ? startPosition = "50%" : startPosition = "0%";
+            i > 0 ? startPosition = "60%" : startPosition = "0%";
             const movingIcon = svg.querySelector("#moving-icon");
             let svgPath = null;
             if (window.innerWidth >= 480) // Desktop
             svgPath = svg.querySelector(".path-container.is-desktop path");
             else // Mobile
             svgPath = svg.querySelector(".path-container.is-mobile path");
+            // Set the transform origin and offsets before animating
+            (0, _gsapDefault.default).set(movingIcon, {
+                xPercent: -50,
+                yPercent: -50,
+                transformOrigin: "50% 50%"
+            });
             const tween = (0, _gsapDefault.default).to(movingIcon, {
                 motionPath: {
                     path: svgPath,
@@ -623,15 +653,14 @@ function initMotionPath() {
                     ],
                     offsetX: 0,
                     offsetY: 0,
-                    start: 0,
-                    end: 1
+                    start: 0
                 },
                 ease: "none",
                 scrollTrigger: {
                     trigger: svg,
                     start: `top ${startPosition}`,
                     end: "bottom 80%",
-                    scrub: 2
+                    scrub: 2.5
                 }
             });
             tweens.push(tween);
@@ -639,19 +668,42 @@ function initMotionPath() {
     }
     // Call createTweens on DOMContentLoaded
     document.addEventListener("DOMContentLoaded", function() {
-        const svgs = document.querySelectorAll(".svg-parent");
-        createTweens(svgs);
+        (0, _imagesloadedDefault.default)(".page-wrapper", ()=>{
+            svgs.forEach((svg)=>{
+                const svgPath = svg.querySelector("path");
+                setViewBox(svg, svgPath);
+            });
+            setTimeout(()=>{
+                createTweens(svgs);
+            }, 1000);
+        });
     });
-    // Update tweens on window resize
-    window.addEventListener("resize", function() {
-        const svgs = document.querySelectorAll(".svg-parent");
+    function debounce(func, delay) {
+        let timeoutId;
+        return function() {
+            const context = this;
+            const args = arguments;
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(()=>{
+                func.apply(context, args);
+            }, delay);
+        };
+    }
+    // Create a debounced version of the resize function
+    const debouncedResize = debounce(function() {
+        svgs.forEach((svg)=>{
+            const svgPath = svg.querySelector("path");
+            setViewBox(svg, svgPath);
+        });
         createTweens(svgs);
-    });
+    }, 200); // Adjust the delay as needed
+    // Update tweens on window resize, using the debounced version
+    window.addEventListener("resize", debouncedResize);
 }
 // Call initMotionPath to initialize motion paths
 initMotionPath();
 
-},{"gsap":"fPSuC","gsap/ScrollTrigger":"7wnFk","gsap/MotionPathPlugin":"3pHNs","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"fPSuC":[function(require,module,exports) {
+},{"gsap":"fPSuC","gsap/ScrollTrigger":"7wnFk","gsap/MotionPathPlugin":"3pHNs","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","imagesloaded":"aYzyZ"}],"fPSuC":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "gsap", ()=>gsapWithCSS);
@@ -8445,6 +8497,349 @@ function getGlobalMatrix(element, inverse, adjustGOffset, includeScrollInFixed) 
     return inverse ? m.inverse() : m;
 }
 
-},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["ctM6v","jHaiW"], "jHaiW", "parcelRequireaff7")
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"aYzyZ":[function(require,module,exports) {
+/*!
+ * imagesLoaded v5.0.0
+ * JavaScript is all like "You images are done yet or what?"
+ * MIT License
+ */ (function(window1, factory) {
+    // universal module definition
+    if (0, module.exports) // CommonJS
+    module.exports = factory(window1, require("493897767f7120e"));
+    else // browser global
+    window1.imagesLoaded = factory(window1, window1.EvEmitter);
+})(typeof window !== "undefined" ? window : this, function factory(window1, EvEmitter) {
+    let $ = window1.jQuery;
+    let console = window1.console;
+    // -------------------------- helpers -------------------------- //
+    // turn element or nodeList into an array
+    function makeArray(obj) {
+        // use object if already an array
+        if (Array.isArray(obj)) return obj;
+        let isArrayLike = typeof obj == "object" && typeof obj.length == "number";
+        // convert nodeList to array
+        if (isArrayLike) return [
+            ...obj
+        ];
+        // array of single index
+        return [
+            obj
+        ];
+    }
+    // -------------------------- imagesLoaded -------------------------- //
+    /**
+ * @param {[Array, Element, NodeList, String]} elem
+ * @param {[Object, Function]} options - if function, use as callback
+ * @param {Function} onAlways - callback function
+ * @returns {ImagesLoaded}
+ */ function ImagesLoaded(elem, options, onAlways) {
+        // coerce ImagesLoaded() without new, to be new ImagesLoaded()
+        if (!(this instanceof ImagesLoaded)) return new ImagesLoaded(elem, options, onAlways);
+        // use elem as selector string
+        let queryElem = elem;
+        if (typeof elem == "string") queryElem = document.querySelectorAll(elem);
+        // bail if bad element
+        if (!queryElem) {
+            console.error(`Bad element for imagesLoaded ${queryElem || elem}`);
+            return;
+        }
+        this.elements = makeArray(queryElem);
+        this.options = {};
+        // shift arguments if no options set
+        if (typeof options == "function") onAlways = options;
+        else Object.assign(this.options, options);
+        if (onAlways) this.on("always", onAlways);
+        this.getImages();
+        // add jQuery Deferred object
+        if ($) this.jqDeferred = new $.Deferred();
+        // HACK check async to allow time to bind listeners
+        setTimeout(this.check.bind(this));
+    }
+    ImagesLoaded.prototype = Object.create(EvEmitter.prototype);
+    ImagesLoaded.prototype.getImages = function() {
+        this.images = [];
+        // filter & find items if we have an item selector
+        this.elements.forEach(this.addElementImages, this);
+    };
+    const elementNodeTypes = [
+        1,
+        9,
+        11
+    ];
+    /**
+ * @param {Node} elem
+ */ ImagesLoaded.prototype.addElementImages = function(elem) {
+        // filter siblings
+        if (elem.nodeName === "IMG") this.addImage(elem);
+        // get background image on element
+        if (this.options.background === true) this.addElementBackgroundImages(elem);
+        // find children
+        // no non-element nodes, #143
+        let { nodeType } = elem;
+        if (!nodeType || !elementNodeTypes.includes(nodeType)) return;
+        let childImgs = elem.querySelectorAll("img");
+        // concat childElems to filterFound array
+        for (let img of childImgs)this.addImage(img);
+        // get child background images
+        if (typeof this.options.background == "string") {
+            let children = elem.querySelectorAll(this.options.background);
+            for (let child of children)this.addElementBackgroundImages(child);
+        }
+    };
+    const reURL = /url\((['"])?(.*?)\1\)/gi;
+    ImagesLoaded.prototype.addElementBackgroundImages = function(elem) {
+        let style = getComputedStyle(elem);
+        // Firefox returns null if in a hidden iframe https://bugzil.la/548397
+        if (!style) return;
+        // get url inside url("...")
+        let matches = reURL.exec(style.backgroundImage);
+        while(matches !== null){
+            let url = matches && matches[2];
+            if (url) this.addBackground(url, elem);
+            matches = reURL.exec(style.backgroundImage);
+        }
+    };
+    /**
+ * @param {Image} img
+ */ ImagesLoaded.prototype.addImage = function(img) {
+        let loadingImage = new LoadingImage(img);
+        this.images.push(loadingImage);
+    };
+    ImagesLoaded.prototype.addBackground = function(url, elem) {
+        let background = new Background(url, elem);
+        this.images.push(background);
+    };
+    ImagesLoaded.prototype.check = function() {
+        this.progressedCount = 0;
+        this.hasAnyBroken = false;
+        // complete if no images
+        if (!this.images.length) {
+            this.complete();
+            return;
+        }
+        /* eslint-disable-next-line func-style */ let onProgress = (image, elem, message)=>{
+            // HACK - Chrome triggers event before object properties have changed. #83
+            setTimeout(()=>{
+                this.progress(image, elem, message);
+            });
+        };
+        this.images.forEach(function(loadingImage) {
+            loadingImage.once("progress", onProgress);
+            loadingImage.check();
+        });
+    };
+    ImagesLoaded.prototype.progress = function(image, elem, message) {
+        this.progressedCount++;
+        this.hasAnyBroken = this.hasAnyBroken || !image.isLoaded;
+        // progress event
+        this.emitEvent("progress", [
+            this,
+            image,
+            elem
+        ]);
+        if (this.jqDeferred && this.jqDeferred.notify) this.jqDeferred.notify(this, image);
+        // check if completed
+        if (this.progressedCount === this.images.length) this.complete();
+        if (this.options.debug && console) console.log(`progress: ${message}`, image, elem);
+    };
+    ImagesLoaded.prototype.complete = function() {
+        let eventName = this.hasAnyBroken ? "fail" : "done";
+        this.isComplete = true;
+        this.emitEvent(eventName, [
+            this
+        ]);
+        this.emitEvent("always", [
+            this
+        ]);
+        if (this.jqDeferred) {
+            let jqMethod = this.hasAnyBroken ? "reject" : "resolve";
+            this.jqDeferred[jqMethod](this);
+        }
+    };
+    // --------------------------  -------------------------- //
+    function LoadingImage(img) {
+        this.img = img;
+    }
+    LoadingImage.prototype = Object.create(EvEmitter.prototype);
+    LoadingImage.prototype.check = function() {
+        // If complete is true and browser supports natural sizes,
+        // try to check for image status manually.
+        let isComplete = this.getIsImageComplete();
+        if (isComplete) {
+            // report based on naturalWidth
+            this.confirm(this.img.naturalWidth !== 0, "naturalWidth");
+            return;
+        }
+        // If none of the checks above matched, simulate loading on detached element.
+        this.proxyImage = new Image();
+        // add crossOrigin attribute. #204
+        if (this.img.crossOrigin) this.proxyImage.crossOrigin = this.img.crossOrigin;
+        this.proxyImage.addEventListener("load", this);
+        this.proxyImage.addEventListener("error", this);
+        // bind to image as well for Firefox. #191
+        this.img.addEventListener("load", this);
+        this.img.addEventListener("error", this);
+        this.proxyImage.src = this.img.currentSrc || this.img.src;
+    };
+    LoadingImage.prototype.getIsImageComplete = function() {
+        // check for non-zero, non-undefined naturalWidth
+        // fixes Safari+InfiniteScroll+Masonry bug infinite-scroll#671
+        return this.img.complete && this.img.naturalWidth;
+    };
+    LoadingImage.prototype.confirm = function(isLoaded, message) {
+        this.isLoaded = isLoaded;
+        let { parentNode } = this.img;
+        // emit progress with parent <picture> or self <img>
+        let elem = parentNode.nodeName === "PICTURE" ? parentNode : this.img;
+        this.emitEvent("progress", [
+            this,
+            elem,
+            message
+        ]);
+    };
+    // ----- events ----- //
+    // trigger specified handler for event type
+    LoadingImage.prototype.handleEvent = function(event) {
+        let method = "on" + event.type;
+        if (this[method]) this[method](event);
+    };
+    LoadingImage.prototype.onload = function() {
+        this.confirm(true, "onload");
+        this.unbindEvents();
+    };
+    LoadingImage.prototype.onerror = function() {
+        this.confirm(false, "onerror");
+        this.unbindEvents();
+    };
+    LoadingImage.prototype.unbindEvents = function() {
+        this.proxyImage.removeEventListener("load", this);
+        this.proxyImage.removeEventListener("error", this);
+        this.img.removeEventListener("load", this);
+        this.img.removeEventListener("error", this);
+    };
+    // -------------------------- Background -------------------------- //
+    function Background(url, element) {
+        this.url = url;
+        this.element = element;
+        this.img = new Image();
+    }
+    // inherit LoadingImage prototype
+    Background.prototype = Object.create(LoadingImage.prototype);
+    Background.prototype.check = function() {
+        this.img.addEventListener("load", this);
+        this.img.addEventListener("error", this);
+        this.img.src = this.url;
+        // check if image is already complete
+        let isComplete = this.getIsImageComplete();
+        if (isComplete) {
+            this.confirm(this.img.naturalWidth !== 0, "naturalWidth");
+            this.unbindEvents();
+        }
+    };
+    Background.prototype.unbindEvents = function() {
+        this.img.removeEventListener("load", this);
+        this.img.removeEventListener("error", this);
+    };
+    Background.prototype.confirm = function(isLoaded, message) {
+        this.isLoaded = isLoaded;
+        this.emitEvent("progress", [
+            this,
+            this.element,
+            message
+        ]);
+    };
+    // -------------------------- jQuery -------------------------- //
+    ImagesLoaded.makeJQueryPlugin = function(jQuery) {
+        jQuery = jQuery || window1.jQuery;
+        if (!jQuery) return;
+        // set local variable
+        $ = jQuery;
+        // $().imagesLoaded()
+        $.fn.imagesLoaded = function(options, onAlways) {
+            let instance = new ImagesLoaded(this, options, onAlways);
+            return instance.jqDeferred.promise($(this));
+        };
+    };
+    // try making plugin
+    ImagesLoaded.makeJQueryPlugin();
+    // --------------------------  -------------------------- //
+    return ImagesLoaded;
+});
+
+},{"493897767f7120e":"7rCHo"}],"7rCHo":[function(require,module,exports) {
+/**
+ * EvEmitter v2.1.1
+ * Lil' event emitter
+ * MIT License
+ */ (function(global, factory) {
+    // universal module definition
+    if (0, module.exports) // CommonJS - Browserify, Webpack
+    module.exports = factory();
+    else // Browser globals
+    global.EvEmitter = factory();
+})(typeof window != "undefined" ? window : this, function() {
+    function EvEmitter() {}
+    let proto = EvEmitter.prototype;
+    proto.on = function(eventName, listener) {
+        if (!eventName || !listener) return this;
+        // set events hash
+        let events = this._events = this._events || {};
+        // set listeners array
+        let listeners = events[eventName] = events[eventName] || [];
+        // only add once
+        if (!listeners.includes(listener)) listeners.push(listener);
+        return this;
+    };
+    proto.once = function(eventName, listener) {
+        if (!eventName || !listener) return this;
+        // add event
+        this.on(eventName, listener);
+        // set once flag
+        // set onceEvents hash
+        let onceEvents = this._onceEvents = this._onceEvents || {};
+        // set onceListeners object
+        let onceListeners = onceEvents[eventName] = onceEvents[eventName] || {};
+        // set flag
+        onceListeners[listener] = true;
+        return this;
+    };
+    proto.off = function(eventName, listener) {
+        let listeners = this._events && this._events[eventName];
+        if (!listeners || !listeners.length) return this;
+        let index = listeners.indexOf(listener);
+        if (index != -1) listeners.splice(index, 1);
+        return this;
+    };
+    proto.emitEvent = function(eventName, args) {
+        let listeners = this._events && this._events[eventName];
+        if (!listeners || !listeners.length) return this;
+        // copy over to avoid interference if .off() in listener
+        listeners = listeners.slice(0);
+        args = args || [];
+        // once stuff
+        let onceListeners = this._onceEvents && this._onceEvents[eventName];
+        for (let listener of listeners){
+            let isOnce = onceListeners && onceListeners[listener];
+            if (isOnce) {
+                // remove listener
+                // remove before trigger to prevent recursion
+                this.off(eventName, listener);
+                // unset once flag
+                delete onceListeners[listener];
+            }
+            // trigger listener
+            listener.apply(this, args);
+        }
+        return this;
+    };
+    proto.allOff = function() {
+        delete this._events;
+        delete this._onceEvents;
+        return this;
+    };
+    return EvEmitter;
+});
+
+},{}]},["ctM6v","jHaiW"], "jHaiW", "parcelRequireaff7")
 
 //# sourceMappingURL=index.js.map
--- src/home/index.js
+++ src/home/index.js
@@ -1,13 +1,41 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import imagesLoaded from "imagesloaded";
 
 gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
+const svgs = document.querySelectorAll(".svg-parent");
+function setViewBox(svg, svgPath) {
+  const svgContainer = svg.parentElement;
+  const svgWidth = svgContainer.clientWidth;
+  const svgHeight = svgContainer.clientHeight;
+
+  // Get the bounding box of the SVG path
+  const pathBounds = svgPath.getBBox();
+  const pathAspectRatio = pathBounds.width / pathBounds.height;
+
+  // Calculate the viewBox dimensions based on the aspect ratio of the path
+  let viewBoxWidth, viewBoxHeight;
+  if (svgWidth / svgHeight > pathAspectRatio) {
+    viewBoxWidth = pathBounds.width;
+    viewBoxHeight = viewBoxWidth * (svgHeight / svgWidth);
+  } else {
+    viewBoxHeight = pathBounds.height;
+    viewBoxWidth = viewBoxHeight * (svgWidth / svgHeight);
+  }
+
+  // Set the viewBox attribute of the SVG
+  svg.setAttribute(
+    "viewBox",
+    `${pathBounds.x} ${pathBounds.y} ${viewBoxWidth} ${viewBoxHeight}`
+  );
+  console.log("viewbox set");
+}
 
 function initMotionPath() {
   // Register MotionPathPlugin here to avoid multiple registrations
   gsap.registerPlugin(MotionPathPlugin, ScrollTrigger);
-
+  ScrollTrigger.normalizeScroll(true);
   // declare an array to store tweens
   let tweens = [];
 
@@ -22,7 +50,7 @@ function initMotionPath() {
     // iterate over each SVG
     svgs.forEach((svg, i) => {
       let startPosition = "0%";
-      i > 0 ? (startPosition = "50%") : (startPosition = "0%");
+      i > 0 ? (startPosition = "60%") : (startPosition = "0%");
       const movingIcon = svg.querySelector("#moving-icon");
 
       let svgPath = null;
@@ -34,6 +62,13 @@ function initMotionPath() {
         svgPath = svg.querySelector(".path-container.is-mobile path");
       }
 
+      // Set the transform origin and offsets before animating
+      gsap.set(movingIcon, {
+        xPercent: -50, // Center horizontally
+        yPercent: -50, // Center vertically
+        transformOrigin: "50% 50%", // Set the transform origin to the center
+      });
+
       const tween = gsap.to(movingIcon, {
         motionPath: {
           path: svgPath,
@@ -43,14 +78,14 @@ function initMotionPath() {
           offsetX: 0,
           offsetY: 0,
           start: 0,
-          end: 1,
+          //end: 1,
         },
         ease: "none",
         scrollTrigger: {
           trigger: svg,
           start: `top ${startPosition}`,
           end: "bottom 80%",
-          scrub: 2,
+          scrub: 2.5,
         },
       });
       tweens.push(tween);
@@ -59,15 +94,40 @@ function initMotionPath() {
 
   // Call createTweens on DOMContentLoaded
   document.addEventListener("DOMContentLoaded", function () {
-    const svgs = document.querySelectorAll(".svg-parent");
-    createTweens(svgs);
+    imagesLoaded(".page-wrapper", () => {
+      svgs.forEach((svg) => {
+        const svgPath = svg.querySelector("path");
+        setViewBox(svg, svgPath);
+      });
+      setTimeout(() => {
+        createTweens(svgs);
+      }, 1000);
+    });
   });
 
-  // Update tweens on window resize
-  window.addEventListener("resize", function () {
-    const svgs = document.querySelectorAll(".svg-parent");
+  function debounce(func, delay) {
+    let timeoutId;
+    return function () {
+      const context = this;
+      const args = arguments;
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        func.apply(context, args);
+      }, delay);
+    };
+  }
+
+  // Create a debounced version of the resize function
+  const debouncedResize = debounce(function () {
+    svgs.forEach((svg) => {
+      const svgPath = svg.querySelector("path");
+      setViewBox(svg, svgPath);
+    });
     createTweens(svgs);
-  });
+  }, 200); // Adjust the delay as needed
+
+  // Update tweens on window resize, using the debounced version
+  window.addEventListener("resize", debouncedResize);
 }
 
 // Call initMotionPath to initialize motion paths
